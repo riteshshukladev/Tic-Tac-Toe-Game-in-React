769b2b36bbd94c4582dbbdff0f69864294b49559
RItesh <shuklaritesh5678@gmail.com>
2023-10-29 01:26:00 +0530

    build a input area that triggers on edit button clicks which is not functioning currently

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react"
+
 export default function Player({name,symbol}) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const clickEventHandler = () => {
+        setIsEditing(true);
+    }
+
+    if(isEditing) return (
+        <li>
+            <label>Input Name:</label>
+            <input type="text" value={name}/>
+            <label>Input Symbol:</label>
+            <input type="text" value={symbol}/>
+            <button>Save</button>
+        </li>
+    )
+    
   return (
     <li>
       <span>
         <span className="player-name">{name}</span>
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={clickEventHandler}>Edit</button>
     </li>
   )
 }
