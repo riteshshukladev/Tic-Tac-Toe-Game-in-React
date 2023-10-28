388ffb998e7c34b3cf31c7042b5c99e85a164ef0
RItesh <shuklaritesh5678@gmail.com>
2023-10-29 01:07:08 +0530

    Some static values to test the design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,26 @@ function App() {
   
 
   return (
-    <p></p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+            </span>
+          </li>
+          <button>Edit</button>
+          <li>
+            <span>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+      </div>
+    </main>
   )
 }
 
