8cbc83075e295e055c163582dc29cd10fddede53
RItesh <shuklaritesh5678@gmail.com>
2023-10-31 23:47:23 +0530

    Fixed the issue of selecting a symbol multiple times

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,11 @@
 export default function Log({turns}) {
     return (
         <ol id="log">
-            
+           { turns.map((turn) =>(
+            <li key={`${turn.square.row}${turn.square.col}`}>
+                {turn.player} played at row {turn.square.row+1} and col {turn.square.col+1}
+            </li>
+           ))}
         </ol>
     )
 }
